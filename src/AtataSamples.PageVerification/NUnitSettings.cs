3fbb114bdbcbe5958b51d4ea750e04035c28ee00
YevgeniyShunevych <eshunevich@gmail.com>
2017-03-15 15:52:25 +0200

    Add ComplexPageDataVerification test

---
+++ src/AtataSamples.PageVerification/NUnitSettings.cs
@@ -0,0 +1,6 @@
+﻿using NUnit.Framework;
+
+[assembly: LevelOfParallelism(4)]
+[assembly: Parallelizable(ParallelScope.Fixtures)]
+[assembly: SetCulture("en-us")]
+[assembly: SetUICulture("en-us")]
--- src/AtataSamples.PageVerification/PlanTests.cs
+++ src/AtataSamples.PageVerification/PlanTests.cs
@@ -7,6 +7,13 @@ namespace AtataSamples.PageVerification
     [TestFixture]
     public class PlanTests
     {
+        private const string Feature1 = "Feature 1";
+        private const string Feature2 = "Feature 2";
+        private const string Feature3 = "Feature 3";
+        private const string Feature4 = "Feature 4";
+        private const string Feature5 = "Feature 5";
+        private const string Feature6 = "Feature 6";
+
         [SetUp]
         public void SetUp()
         {
@@ -47,6 +54,28 @@ namespace AtataSamples.PageVerification
         public void PrimaryPageDataVerification_DynamicTriggers()
         {
             Go.To<PlansWithDynamicTriggersPage>();
+        }
+
+        [Test]
+        public void ComplexPageDataVerification()
+        {
+            Go.To<PlansPage>().
+                PlanItems.Count.Should.Equal(3).
+
+                PlanItems[0].Title.Should.Equal("Basic").
+                PlanItems[0].Price.Should.Equal(0).
+                PlanItems[0].NumberOfProjects.Should.Equal(1).
+                PlanItems[0].Features.Should.Contain(Feature1, Feature2).
+
+                PlanItems[1].Title.Should.Equal("Plus").
+                PlanItems[1].Price.Should.Equal(19.99m).
+                PlanItems[1].NumberOfProjects.Should.Equal(3).
+                PlanItems[1].Features.Should.Contain(Feature1, Feature2, Feature3, Feature4).
+
+                PlanItems[2].Title.Should.Equal("Premium").
+                PlanItems[2].Price.Should.Equal(49.99m).
+                PlanItems[2].NumberOfProjects.Should.Equal(10).
+                PlanItems[2].Features.Should.Contain(Feature1, Feature2, Feature3, Feature4, Feature5, Feature6);
         }
     }
 }
