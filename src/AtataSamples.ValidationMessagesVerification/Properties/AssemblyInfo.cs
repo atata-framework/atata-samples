65398b2f09dcda489f8698e1164e6b4666298344
YevgeniyShunevych <eshunevich@gmail.com>
2017-03-30 16:07:07 +0300

    Add initial AtataSamples.ValidationMessagesVerification project; Add AtataSamples solution

---
+++ src/AtataSamples.ValidationMessagesVerification/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Yevgeniy Shunevych")]
+[assembly: AssemblyProduct("Atata Framework Samples")]
+[assembly: AssemblyCopyright("Copyright © Yevgeniy Shunevych 2017")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyTitle("AtataSamples.ValidationMessagesVerification")]
+[assembly: Guid("62a7531b-bb9b-4dac-a874-5945d89e8d60")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
