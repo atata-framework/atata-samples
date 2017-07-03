754307077468b59f5ebd9d12d4071e795479e2ea
YevgeniyShunevych <eshunevich@gmail.com>
2017-07-03 17:10:22 +0300

    Add AtataSamples.ConfirmationPopups project

---
+++ src/AtataSamples.ConfirmationPopups/Components/ProductsPage.cs
@@ -0,0 +1,31 @@
+﻿using Atata;
+
+namespace AtataSamples.ConfirmationPopups
+{
+    using _ = ProductsPage;
+
+    [Url("products")]
+    public class ProductsPage : Page<_>
+    {
+        public Table<ProductTableRow, _> Products { get; private set; }
+
+        public class ProductTableRow : TableRow<_>
+        {
+            public Text<_> Name { get; private set; }
+
+            public Currency<_> Price { get; private set; }
+
+            public Number<_> Amount { get; private set; }
+
+            [CloseConfirmBox]
+            public ButtonDelegate<_> DeleteUsingJSConfirm { get; private set; }
+
+            [Term("Delete Using BS Modal")]
+            public ButtonDelegate<BSDeleteConfirmationModal<_>, _> DeleteUsingBSModal { get; private set; }
+
+            [Term("Delete Using BS Modal")]
+            [ConfirmBSDeletion]
+            public ButtonDelegate<_> DeleteUsingBSModalViaTrigger { get; private set; }
+        }
+    }
+}
---
+++ src/AtataSamples.ConfirmationPopups/UITestFixture.cs
@@ -0,0 +1,29 @@
+﻿using Atata;
+using NUnit.Framework;
+
+namespace AtataSamples.ConfirmationPopups
+{
+    [TestFixture]
+    public class UITestFixture
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            AtataContext.Build().
+                UseChrome().
+                    WithArguments("disable-extensions", "no-sandbox", "start-maximized").
+                UseBaseUrl("https://atata-framework.github.io/atata-sample-app/#!/").
+                UseNUnitTestName().
+                AddNUnitTestContextLogging().
+                    WithoutSectionFinish().
+                LogNUnitError().
+                SetUp();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            AtataContext.Current.CleanUp();
+        }
+    }
+}
