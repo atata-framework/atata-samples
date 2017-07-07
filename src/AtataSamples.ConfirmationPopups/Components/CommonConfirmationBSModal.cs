8f34a3dde5af64edc8a16a1bda95236e61a8e0fd
YevgeniyShunevych <eshunevich@gmail.com>
2017-07-07 18:12:36 +0300

    Add CommonConfirmationBSModal

---
+++ src/AtataSamples.ConfirmationPopups/Components/CommonConfirmationBSModal.cs
@@ -0,0 +1,16 @@
+﻿using Atata;
+using Atata.Bootstrap;
+
+namespace AtataSamples.ConfirmationPopups
+{
+    using _ = CommonConfirmationBSModal;
+
+    public class CommonConfirmationBSModal : BSModal<_>
+    {
+        [FindByClass("btn-primary")]
+        public ButtonDelegate<_> Confirm { get; private set; }
+
+        [FindByClass("btn-default")]
+        public ButtonDelegate<_> Cancel { get; private set; }
+    }
+}
