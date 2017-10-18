722632878a1dc815a4315142d0a3a8ec405eaf27
YevgeniyShunevych <eshunevich@gmail.com>
2017-10-18 11:29:19 +0300

    Rename IValidationMessageVerificationProviderExtensions to ValidationMessageExtensions

---
+++ src/AtataSamples.ValidationMessagesVerification/Extensions/ValidationMessageExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Atata;
+
+namespace AtataSamples.ValidationMessagesVerification
+{
+    public static class ValidationMessageExtensions
+    {
+        public static TOwner BeRequired<TOwner>(this IFieldVerificationProvider<string, ValidationMessage<TOwner>, TOwner> should)
+            where TOwner : PageObject<TOwner>
+        {
+            return should.Equal("is required");
+        }
+
+        public static TOwner HaveIncorrectFormat<TOwner>(this IFieldVerificationProvider<string, ValidationMessage<TOwner>, TOwner> should)
+            where TOwner : PageObject<TOwner>
+        {
+            return should.Equal("has incorrect format");
+        }
+
+        public static TOwner HaveMinLength<TOwner>(this IFieldVerificationProvider<string, ValidationMessage<TOwner>, TOwner> should, int length)
+            where TOwner : PageObject<TOwner>
+        {
+            return should.Equal($"minimum length is {length}");
+        }
+
+        public static TOwner HaveMaxLength<TOwner>(this IFieldVerificationProvider<string, ValidationMessage<TOwner>, TOwner> should, int length)
+            where TOwner : PageObject<TOwner>
+        {
+            return should.Equal($"maximum length is {length}");
+        }
+    }
+}
