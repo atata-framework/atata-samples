f8714b0fffccaafd2eb5e909cd12991146b8dfc0
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2019-12-12 14:25:28 +0200

    Update ParallelTestsReusingDrivers sample project

--- ParallelTestsReusingDrivers/AtataSamples.ParallelTestsReusingDrivers/DriverPool.cs
+++ ParallelTestsReusingDrivers/AtataSamples.ParallelTestsReusingDrivers/DriverPool.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using Atata;
 using OpenQA.Selenium.Remote;
 
@@ -8,23 +10,70 @@ namespace AtataSamples.ParallelTestsReusingDrivers
 {
     public static class DriverPool
     {
-        private static readonly ConcurrentBag<DriverEntry> Entries = new ConcurrentBag<DriverEntry>();
+        private static readonly ConcurrentBag<DriverEntry> GlobalEntries = new ConcurrentBag<DriverEntry>();
 
-        public static RemoteWebDriver Acquire(IDriverFactory driverFactory)
+        private static readonly ConcurrentDictionary<object, ConcurrentBag<DriverEntry>> ScopedEntries = new ConcurrentDictionary<object, ConcurrentBag<DriverEntry>>();
+
+        private static IEnumerable<ConcurrentBag<DriverEntry>> AllEntryBags =>
+            new[] { GlobalEntries }.Concat(ScopedEntries.Values);
+
+        private static IEnumerable<DriverEntry> AllEntries =>
+            AllEntryBags.SelectMany(x => x);
+
+        /// <summary>
+        /// Acquires the driver for the speific <paramref name="driverFactory"/>.
+        /// </summary>
+        /// <param name="driverFactory">The driver factory.</param>
+        /// <param name="poolScopeObject">
+        /// The pool scope object.
+        /// Is optional.
+        /// Mostly can be a fixture class object.
+        /// When is <see langword="null"/> then will use whole global common pool;
+        /// otherwise will use separate pool for particular scope object.
+        /// </param>
+        /// <returns>A <see cref="RemoteWebDriver"/> created or gotten from pool.</returns>
+        /// <exception cref="ArgumentNullException">driverFactory</exception>
+        public static RemoteWebDriver Acquire(IDriverFactory driverFactory, object poolScopeObject = null)
         {
             if (driverFactory == null)
                 throw new ArgumentNullException(nameof(driverFactory));
 
-            DriverEntry entry = Entries.FirstOrDefault(x => x.Alias == driverFactory.Alias && !x.IsAcquired);
+            ConcurrentBag<DriverEntry> entries = ResolveEntriesBag(poolScopeObject);
 
-            if (entry == null)
+            Monitor.Enter(entries);
+
+            try
             {
-                entry = CreateDriverEntry(driverFactory);
-                Entries.Add(entry);
-            }
+                DriverEntry entry = entries.FirstOrDefault(x => x.Alias == driverFactory.Alias && !x.IsAcquired);
 
-            entry.IsAcquired = true;
-            return entry.Driver;
+                if (entry is null)
+                {
+                    Monitor.Exit(entries);
+
+                    entry = CreateDriverEntry(driverFactory);
+                    entry.IsAcquired = true;
+
+                    entries.Add(entry);
+                }
+                else
+                {
+                    entry.IsAcquired = true;
+                }
+
+                return entry.Driver;
+            }
+            finally
+            {
+                if (Monitor.IsEntered(entries))
+                    Monitor.Exit(entries);
+            }
+        }
+
+        private static ConcurrentBag<DriverEntry> ResolveEntriesBag(object poolScopeObject)
+        {
+            return poolScopeObject == null
+                ? GlobalEntries
+                : ScopedEntries.GetOrAdd(poolScopeObject, _ => new ConcurrentBag<DriverEntry>());
         }
 
         private static DriverEntry CreateDriverEntry(IDriverFactory driverFactory)
@@ -36,15 +85,31 @@ namespace AtataSamples.ParallelTestsReusingDrivers
 
         public static void Release(RemoteWebDriver driver)
         {
-            DriverEntry entry = Entries.FirstOrDefault(x => x.Driver == driver);
+            DriverEntry entry = AllEntries.FirstOrDefault(x => x.Driver == driver);
 
             if (entry != null)
                 entry.IsAcquired = false;
         }
 
+        public static void CloseAllForScope(object poolScopeObject)
+        {
+            if (ScopedEntries.TryRemove(poolScopeObject, out var entries))
+            {
+                Close(entries);
+            }
+        }
+
         public static void CloseAll()
         {
-            foreach (DriverEntry entry in Entries)
+            foreach (var entries in AllEntryBags)
+                Close(entries);
+
+            ScopedEntries.Clear();
+        }
+
+        private static void Close(ConcurrentBag<DriverEntry> entries)
+        {
+            foreach (DriverEntry entry in entries)
             {
                 try
                 {
@@ -55,7 +120,7 @@ namespace AtataSamples.ParallelTestsReusingDrivers
                 }
             }
 
-            Entries.Clear();
+            entries.Clear();
         }
 
         private class DriverEntry
--- ParallelTestsReusingDrivers/AtataSamples.ParallelTestsReusingDrivers/Extensions/AtataContextBuilderExtensions.cs
+++ ParallelTestsReusingDrivers/AtataSamples.ParallelTestsReusingDrivers/Extensions/AtataContextBuilderExtensions.cs
@@ -5,11 +5,23 @@ namespace AtataSamples.ParallelTestsReusingDrivers
 {
     public static class AtataContextBuilderExtensions
     {
-        public static AtataContextBuilder UseDriverPool(this AtataContextBuilder builder)
+        /// <summary>
+        /// Use the driver pool with optionally resricting the pool with <paramref name="poolScopeObject"/>.
+        /// </summary>
+        /// <param name="builder">An <see cref="AtataContextBuilder"/>.</param>
+        /// <param name="poolScopeObject">
+        /// The pool scope object.
+        /// Is optional.
+        /// Mostly can be a fixture class object.
+        /// When is <see langword="null"/> then will use whole global common pool;
+        /// otherwise will use separate pool for particular scope object.
+        /// </param>
+        /// <returns></returns>
+        public static AtataContextBuilder UseDriverPool(this AtataContextBuilder builder, object poolScopeObject = null)
         {
             IDriverFactory driverFactory = builder.BuildingContext.DriverFactoryToUse;
 
-            RemoteWebDriver driver = DriverPool.Acquire(driverFactory);
+            RemoteWebDriver driver = DriverPool.Acquire(driverFactory, poolScopeObject);
 
             return builder.UseDriver(driver).
                 OnCleanUp(ReleaseCurrentDriver);
