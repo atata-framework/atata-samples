081c1e67c401575e6d763fd05ae80651eee5c5be
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2020-08-06 16:18:35 +0300

    Upgrade AtataSamples.MultipleBrowsersViaFixtureArguments project

--- MultipleBrowsersViaFixtureArguments/AtataSamples.MultipleBrowsersViaFixtureArguments/SetUpFixture.cs
+++ MultipleBrowsersViaFixtureArguments/AtataSamples.MultipleBrowsersViaFixtureArguments/SetUpFixture.cs
@@ -13,20 +13,21 @@ namespace AtataSamples.MultipleBrowsersViaFixtureArguments
             AtataContext.GlobalConfiguration.
                 UseChrome().
                     WithArguments("start-maximized").
+                    WithDriverPath(Environment.GetEnvironmentVariable("ChromeDriver") ?? AppDomain.CurrentDomain.BaseDirectory).
                 UseInternetExplorer().
                     // TODO: Specify Internet Explorer settings, like:
                     // WithOptions(x => x.EnableNativeEvents = true).
                     WithDriverPath(Environment.GetEnvironmentVariable("IEWebDriver") ?? AppDomain.CurrentDomain.BaseDirectory).
                 UseFirefox().
-                    // TODO: You can also specify remote driver configuration(s):
+                    WithFixOfCommandExecutionDelay().
                     WithDriverPath(Environment.GetEnvironmentVariable("GeckoWebDriver") ?? AppDomain.CurrentDomain.BaseDirectory).
+                // TODO: You can also specify remote driver configuration(s):
                 // UseRemoteDriver().
                 // WithAlias("chrome_remote").
                 // WithRemoteAddress("http://127.0.0.1:4444/wd/hub").
                 // WithOptions(new ChromeOptions()).
                 UseBaseUrl("https://demo.atata.io/").
-                AddNUnitTestContextLogging().
-                LogNUnitError();
+                UseAllNUnitFeatures();
         }
     }
 }
