c2c31f37c4ba2c7cf03c37ef7a6e2b59093d70ed
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2020-08-25 19:20:29 +0300

    Add archiveall.ps1 script and _archives folder

---
+++ archiveall.ps1
@@ -0,0 +1,9 @@
+git clean -dfX
+
+$sampleFolders = Get-ChildItem . -Directory -Exclude ("_archives", ".git", ".vs")
+
+ForEach ($folder in $sampleFolders) 
+{
+	$zipPath = "./_archives/" + $folder.name + ".zip"
+	Compress-Archive -Path $folder -DestinationPath $zipPath -Update
+}
