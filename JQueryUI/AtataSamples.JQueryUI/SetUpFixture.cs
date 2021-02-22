bfb42d751c3959339941dcc5874a7cc675e9b699
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2021-02-22 18:56:02 +0200

    Update AtataSamples.JQueryUI project to use Atata.WebDriverSetup package instead of Selenium.WebDriver.ChromeDriver

---
+++ JQueryUI/AtataSamples.JQueryUI/SetUpFixture.cs
@@ -0,0 +1,22 @@
+﻿using Atata;
+using NUnit.Framework;
+
+namespace AtataSamples.JQueryUI
+{
+    [SetUpFixture]
+    public class SetUpFixture
+    {
+        [OneTimeSetUp]
+        public void GlobalSetUp()
+        {
+            AtataContext.GlobalConfiguration
+                .UseChrome()
+                    .WithArguments("start-maximized")
+                .UseBaseUrl("https://jqueryui.com/")
+                .UseCulture("en-US")
+                .UseAllNUnitFeatures();
+
+            AtataContext.GlobalConfiguration.AutoSetUpDriverToUse();
+        }
+    }
+}
--- JQueryUI/AtataSamples.JQueryUI/UITestFixture.cs
+++ JQueryUI/AtataSamples.JQueryUI/UITestFixture.cs
@@ -9,14 +9,7 @@ namespace AtataSamples.JQueryUI
         [SetUp]
         public void SetUp()
         {
-            AtataContext.Configure().
-                UseChrome().
-                    WithArguments("start-maximized").
-                    WithLocalDriverPath().
-                UseBaseUrl("https://jqueryui.com/").
-                UseCulture("en-US").
-                UseAllNUnitFeatures().
-                Build();
+            AtataContext.Configure().Build();
         }
 
         [TearDown]
