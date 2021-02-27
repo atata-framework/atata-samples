76f09318a39c88712d6cd91a83d09faf5ed74633
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2021-02-27 09:37:04 +0200

    Update AtataSamples.TableWithRowSpannedCells project to use Atata.WebDriverSetup package instead of Selenium.WebDriver.ChromeDriver

---
+++ TableWithRowSpannedCells/AtataSamples.TableWithRowSpannedCells/SetUpFixture.cs
@@ -0,0 +1,22 @@
+﻿using Atata;
+using NUnit.Framework;
+
+namespace AtataSamples.TableWithRowSpannedCells
+{
+    [SetUpFixture]
+    public class SetUpFixture
+    {
+        [OneTimeSetUp]
+        public void GlobalSetUp()
+        {
+            AtataContext.GlobalConfiguration
+                .UseChrome()
+                    .WithArguments("start-maximized")
+                .UseBaseUrl("https://demo.atata.io/")
+                .UseCulture("en-US")
+                .UseAllNUnitFeatures();
+
+            AtataContext.GlobalConfiguration.AutoSetUpDriverToUse();
+        }
+    }
+}
--- TableWithRowSpannedCells/AtataSamples.TableWithRowSpannedCells/TableWithRowSpannedCellsTests.cs
+++ TableWithRowSpannedCells/AtataSamples.TableWithRowSpannedCells/TableWithRowSpannedCellsTests.cs
@@ -9,100 +9,100 @@ namespace AtataSamples.TableWithRowSpannedCells
         [Test]
         public void UsingXPath()
         {
-            Go.To<TableUsingXPathPage>().
-                Users.Rows.Should.HaveCount(3).
+            Go.To<TableUsingXPathPage>()
+                .Users.Rows.Should.HaveCount(3)
 
-                Users.Rows[0].Name.Should.Equal("John Smith").
-                Users.Rows[1].Name.Should.Equal("John Smith").
-                Users.Rows[2].Name.Should.Equal("Total").
+                .Users.Rows[0].Name.Should.Equal("John Smith")
+                .Users.Rows[1].Name.Should.Equal("John Smith")
+                .Users.Rows[2].Name.Should.Equal("Total")
 
-                Users.Rows[0].StartDate.Should.Equal(new DateTime(2016, 7, 7)).
-                Users.Rows[1].StartDate.Should.BeGreater(new DateTime(2016, 1, 1)).
-                Users.Rows[2].StartDate.Should.BeNull().
+                .Users.Rows[0].StartDate.Should.Equal(new DateTime(2016, 7, 7))
+                .Users.Rows[1].StartDate.Should.BeGreater(new DateTime(2016, 1, 1))
+                .Users.Rows[2].StartDate.Should.BeNull()
 
-                Users.Rows[0].ExpertiseLevel.Should.Equal("Architect").
-                Users.Rows[1].ExpertiseLevel.Should.Equal("Architect").
-                Users.Rows[2].ExpertiseLevel.Should.BeNull().
+                .Users.Rows[0].ExpertiseLevel.Should.Equal("Architect")
+                .Users.Rows[1].ExpertiseLevel.Should.Equal("Architect")
+                .Users.Rows[2].ExpertiseLevel.Should.BeNull()
 
-                Users.Rows[0].Client.Should.Equal("SomeSoft").
-                Users.Rows[1].Client.Should.Equal("Unassigned").
-                Users.Rows[2].Client.Should.BeNull().
+                .Users.Rows[0].Client.Should.Equal("SomeSoft")
+                .Users.Rows[1].Client.Should.Equal("Unassigned")
+                .Users.Rows[2].Client.Should.BeNull()
 
-                Users.Rows[0].Project.Should.Equal("BioFruit").
-                Users.Rows[1].Project.Should.Equal("Unassigned").
-                Users.Rows[2].Project.Should.BeNull().
+                .Users.Rows[0].Project.Should.Equal("BioFruit")
+                .Users.Rows[1].Project.Should.Equal("Unassigned")
+                .Users.Rows[2].Project.Should.BeNull()
 
-                Users.Rows.SelectData(x => x.DirectProjectCost).Should.EqualSequence(1693.42m, 564.47m, 2257.89m).
+                .Users.Rows.SelectData(x => x.DirectProjectCost).Should.EqualSequence(1693.42m, 564.47m, 2257.89m)
 
-                Users.Rows[x => x.Name == "John Smith" && x.Client == "Unassigned" && x.Project == "Unassigned"].Should.Exist().
-                Users.Rows[x => x.Name == "John Smith" && x.Client == "SomeSoft"].Project.Should.Equal("BioFruit").
-                Users.Rows[x => x.Name == "Total"].GrossMarginPercent.Should.Equal(0.36m);
+                .Users.Rows[x => x.Name == "John Smith" && x.Client == "Unassigned" && x.Project == "Unassigned"].Should.Exist()
+                .Users.Rows[x => x.Name == "John Smith" && x.Client == "SomeSoft"].Project.Should.Equal("BioFruit")
+                .Users.Rows[x => x.Name == "Total"].GrossMarginPercent.Should.Equal(0.36m);
         }
 
         [Test]
         public void UsingCustomFindAttributes()
         {
-            Go.To<TableUsingCustomFindAttributesPage>().
-                Users.Rows.Should.HaveCount(3).
+            Go.To<TableUsingCustomFindAttributesPage>()
+                .Users.Rows.Should.HaveCount(3)
 
-                Users.Rows[0].Name.Should.Equal("John Smith").
-                Users.Rows[1].Name.Should.Equal("John Smith").
-                Users.Rows[2].Name.Should.Equal("Total").
+                .Users.Rows[0].Name.Should.Equal("John Smith")
+                .Users.Rows[1].Name.Should.Equal("John Smith")
+                .Users.Rows[2].Name.Should.Equal("Total")
 
-                Users.Rows[0].StartDate.Should.Equal(new DateTime(2016, 7, 7)).
-                Users.Rows[1].StartDate.Should.BeGreater(new DateTime(2016, 1, 1)).
-                Users.Rows[2].StartDate.Should.BeNull().
+                .Users.Rows[0].StartDate.Should.Equal(new DateTime(2016, 7, 7))
+                .Users.Rows[1].StartDate.Should.BeGreater(new DateTime(2016, 1, 1))
+                .Users.Rows[2].StartDate.Should.BeNull()
 
-                Users.Rows[0].ExpertiseLevel.Should.Equal("Architect").
-                Users.Rows[1].ExpertiseLevel.Should.Equal("Architect").
-                Users.Rows[2].ExpertiseLevel.Should.BeNull().
+                .Users.Rows[0].ExpertiseLevel.Should.Equal("Architect")
+                .Users.Rows[1].ExpertiseLevel.Should.Equal("Architect")
+                .Users.Rows[2].ExpertiseLevel.Should.BeNull()
 
-                Users.Rows[0].Client.Should.Equal("SomeSoft").
-                Users.Rows[1].Client.Should.Equal("Unassigned").
-                Users.Rows[2].Client.Should.BeNull().
+                .Users.Rows[0].Client.Should.Equal("SomeSoft")
+                .Users.Rows[1].Client.Should.Equal("Unassigned")
+                .Users.Rows[2].Client.Should.BeNull()
 
-                Users.Rows[0].Project.Should.Equal("BioFruit").
-                Users.Rows[1].Project.Should.Equal("Unassigned").
-                Users.Rows[2].Project.Should.BeNull().
+                .Users.Rows[0].Project.Should.Equal("BioFruit")
+                .Users.Rows[1].Project.Should.Equal("Unassigned")
+                .Users.Rows[2].Project.Should.BeNull()
 
-                Users.Rows.SelectData(x => x.DirectProjectCost).Should.EqualSequence(1693.42m, 564.47m, 2257.89m).
+                .Users.Rows.SelectData(x => x.DirectProjectCost).Should.EqualSequence(1693.42m, 564.47m, 2257.89m)
 
-                Users.Rows[x => x.Name == "John Smith" && x.Client == "Unassigned" && x.Project == "Unassigned"].Should.Exist().
-                Users.Rows[x => x.Name == "John Smith" && x.Client == "SomeSoft"].Project.Should.Equal("BioFruit").
-                Users.Rows[x => x.Name == "Total"].GrossMarginPercent.Should.Equal(0.36m);
+                .Users.Rows[x => x.Name == "John Smith" && x.Client == "Unassigned" && x.Project == "Unassigned"].Should.Exist()
+                .Users.Rows[x => x.Name == "John Smith" && x.Client == "SomeSoft"].Project.Should.Equal("BioFruit")
+                .Users.Rows[x => x.Name == "Total"].GrossMarginPercent.Should.Equal(0.36m);
         }
 
         [Test]
         public void UsingCustomFindStrategy()
         {
-            Go.To<TableUsingCustomFindStrategyPage>().
-                Users.Rows.Should.HaveCount(3).
+            Go.To<TableUsingCustomFindStrategyPage>()
+                .Users.Rows.Should.HaveCount(3)
 
-                Users.Rows[0].Name.Should.Equal("John Smith").
-                Users.Rows[1].Name.Should.Equal("John Smith").
-                Users.Rows[2].Name.Should.Equal("Total").
+                .Users.Rows[0].Name.Should.Equal("John Smith")
+                .Users.Rows[1].Name.Should.Equal("John Smith")
+                .Users.Rows[2].Name.Should.Equal("Total")
 
-                Users.Rows[0].StartDate.Should.Equal(new DateTime(2016, 7, 7)).
-                Users.Rows[1].StartDate.Should.BeGreater(new DateTime(2016, 1, 1)).
-                Users.Rows[2].StartDate.Should.BeNull().
+                .Users.Rows[0].StartDate.Should.Equal(new DateTime(2016, 7, 7))
+                .Users.Rows[1].StartDate.Should.BeGreater(new DateTime(2016, 1, 1))
+                .Users.Rows[2].StartDate.Should.BeNull()
 
-                Users.Rows[0].ExpertiseLevel.Should.Equal("Architect").
-                Users.Rows[1].ExpertiseLevel.Should.Equal("Architect").
-                Users.Rows[2].ExpertiseLevel.Should.BeNull().
+                .Users.Rows[0].ExpertiseLevel.Should.Equal("Architect")
+                .Users.Rows[1].ExpertiseLevel.Should.Equal("Architect")
+                .Users.Rows[2].ExpertiseLevel.Should.BeNull()
 
-                Users.Rows[0].Client.Should.Equal("SomeSoft").
-                Users.Rows[1].Client.Should.Equal("Unassigned").
-                Users.Rows[2].Client.Should.BeNull().
+                .Users.Rows[0].Client.Should.Equal("SomeSoft")
+                .Users.Rows[1].Client.Should.Equal("Unassigned")
+                .Users.Rows[2].Client.Should.BeNull()
 
-                Users.Rows[0].Project.Should.Equal("BioFruit").
-                Users.Rows[1].Project.Should.Equal("Unassigned").
-                Users.Rows[2].Project.Should.BeNull().
+                .Users.Rows[0].Project.Should.Equal("BioFruit")
+                .Users.Rows[1].Project.Should.Equal("Unassigned")
+                .Users.Rows[2].Project.Should.BeNull()
 
-                Users.Rows.SelectData(x => x.DirectProjectCost).Should.EqualSequence(1693.42m, 564.47m, 2257.89m).
+                .Users.Rows.SelectData(x => x.DirectProjectCost).Should.EqualSequence(1693.42m, 564.47m, 2257.89m)
 
-                Users.Rows[x => x.Name == "John Smith" && x.Client == "Unassigned" && x.Project == "Unassigned"].Should.Exist().
-                Users.Rows[x => x.Name == "John Smith" && x.Client == "SomeSoft"].Project.Should.Equal("BioFruit").
-                Users.Rows[x => x.Name == "Total"].GrossMarginPercent.Should.Equal(0.36m);
+                .Users.Rows[x => x.Name == "John Smith" && x.Client == "Unassigned" && x.Project == "Unassigned"].Should.Exist()
+                .Users.Rows[x => x.Name == "John Smith" && x.Client == "SomeSoft"].Project.Should.Equal("BioFruit")
+                .Users.Rows[x => x.Name == "Total"].GrossMarginPercent.Should.Equal(0.36m);
         }
     }
 }
