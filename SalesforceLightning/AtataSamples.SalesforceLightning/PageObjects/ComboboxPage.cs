a49541c7ef95fac4d0b74687865f5f3f3abca559
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2023-06-22 17:11:27 +0200

    Upgrade Atata package to v2.8.1

--- SalesforceLightning/AtataSamples.SalesforceLightning/PageObjects/ComboboxPage.cs
+++ SalesforceLightning/AtataSamples.SalesforceLightning/PageObjects/ComboboxPage.cs
@@ -21,7 +21,4 @@ public class ComboboxPage : Page<_>
     [FindByName("progress")]
     [CloseAlertBox(TriggerEvents.AfterSet)]
     public SLCombobox<Progress, _> EnumBasedCombobox { get; private set; }
-
-    protected override void OnInitCompleted() =>
-        Find<Button<_>>(new FindByIdAttribute("onetrust-accept-btn-handler")).Click();
 }
