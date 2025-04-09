a2c67ffde9f2c6bbeb88751c8d1d4ac3e25f0408
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-04-09 09:05:47 +0200

    Fix code analysis warning

--- ExtentReports/AtataSamples.ExtentReports/GlobalSuppressions.cs
+++ ExtentReports/AtataSamples.ExtentReports/GlobalSuppressions.cs
@@ -3,5 +3,4 @@
 #pragma warning disable S103 // Lines should not be too long
 
 [assembly: SuppressMessage("Critical Code Smell", "S134:Control flow statements \"if\", \"switch\", \"for\", \"foreach\", \"while\", \"do\"  and \"try\" should not be nested too deeply", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.ExtentReports.ExtentLogConsumer.ResolveLogStatus(Atata.LogEventInfo)~AventStack.ExtentReports.Status")]
-
-#pragma warning restore S103 // Lines should not be too long
+[assembly: SuppressMessage("Minor Code Smell", "S3236:Caller information arguments should not be provided explicitly", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.ExtentReports.ExtentLogConsumer.ResolveLogStatus(Atata.LogEventInfo)~AventStack.ExtentReports.Status")]
