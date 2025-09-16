393adce5069cfc17cd2599d8353af814ea1960db
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-09-16 12:02:20 +0200

    [AtataSamples.JQueryUI] Add `JQGridFindByColumnHeaderStrategy` and `JQGridFindByColumnHeaderAttribute`

---
+++ JQueryUI/AtataSamples.JQueryUI/Attributes/JQGridFindByColumnHeaderAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Atata;
+
+namespace AtataSamples.JQueryUI;
+
+public sealed class JQGridFindByColumnHeaderAttribute : FindByColumnHeaderAttribute
+{
+    protected override Type DefaultStrategy =>
+        typeof(JQGridFindByColumnHeaderStrategy);
+}
---
+++ JQueryUI/AtataSamples.JQueryUI/Attributes/JQGridFindByColumnHeaderStrategy.cs
@@ -0,0 +1,11 @@
+﻿using Atata;
+
+namespace AtataSamples.JQueryUI;
+
+public sealed class JQGridFindByColumnHeaderStrategy : FindByColumnHeaderStrategy
+{
+    public JQGridFindByColumnHeaderStrategy()
+        : base("ancestor::*[contains(concat(' ', normalize-space(@class), ' '), ' ui-jqgrid-view ')][1]//table[contains(concat(' ', normalize-space(@class), ' '), ' ui-jqgrid-htable ')][1]//th")
+    {
+    }
+}
