cc95e16ffb83c0edeb0c3419ccda38a3100f4472
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-12-01 08:45:00 +0100

    In `AtataSamples.MultipleWebDriverSessionsInTest.SampleTests` add tests: `CreateTwoSessionsUsingAttribute_NavigateThroughSessions` and `CreateTwoSessionsUsingAttribute_NavigateThroughGoToAfterManualSessionSwitch`

--- MultipleWebDriverSessionsInTest/AtataSamples.MultipleWebDriverSessionsInTest/SampleTests.cs
+++ MultipleWebDriverSessionsInTest/AtataSamples.MultipleWebDriverSessionsInTest/SampleTests.cs
@@ -3,6 +3,62 @@
 public sealed class SampleTests : AtataTestSuite
 {
     [Test]
+    [StartSession(typeof(WebDriverSession), Count = 2)] // Tells Atata to start 2 sessions automatically for this test.
+    public void CreateTwoSessionsUsingAttribute_NavigateThroughSessions()
+    {
+        // Gets a first session.
+        var session1 = Context.Sessions.Get<WebDriverSession>(0);
+
+        // Gets a second session.
+        var session2 = Context.Sessions.Get<WebDriverSession>(1);
+
+        // Navigate to "Products" page in scope of the first session.
+        var productsPage = session1.Go.To<OrdinaryPage>(url: "products")
+            .PageTitle.Should.Contain("Products"); // Verify its title.
+
+        // Navigate to "Calculations" page in scope of the second session.
+        var calculationsPage = session2.Go.To<OrdinaryPage>(url: "calculations")
+            .PageTitle.Should.Contain("Calculations"); // Verify its title.
+
+        // Assert page title of the first session page.
+        productsPage.PageTitle.Should.Contain("Products");
+
+        // Assert page title of the second session page.
+        calculationsPage.PageTitle.Should.Contain("Calculations");
+    }
+
+    [Test]
+    [StartSession(typeof(WebDriverSession), Count = 2)] // Tells Atata to start 2 sessions automatically for this test.
+    public void CreateTwoSessionsUsingAttribute_NavigateThroughGoToAfterManualSessionSwitch()
+    {
+        // Gets a first session.
+        var session1 = Context.Sessions.Get<WebDriverSession>(0);
+
+        // Gets a second session.
+        var session2 = Context.Sessions.Get<WebDriverSession>(1);
+
+        // Switch to the first session.
+        session1.SetAsCurrent();
+
+        // Navigate to "Products" page in scope of the first session.
+        var productsPage = Go.To<OrdinaryPage>(url: "products")
+            .PageTitle.Should.Contain("Products"); // Verify its title.
+
+        // Switch to the second session.
+        session2.SetAsCurrent();
+
+        // Navigate to "Calculations" page in scope of the second session.
+        var calculationsPage = Go.To<OrdinaryPage>(url: "calculations")
+            .PageTitle.Should.Contain("Calculations"); // Verify its title.
+
+        // Assert page title of the first session page.
+        productsPage.PageTitle.Should.Contain("Products");
+
+        // Assert page title of the second session page.
+        calculationsPage.PageTitle.Should.Contain("Calculations");
+    }
+
+    [Test]
     public async Task CreateSecondSessionDuringTest_NavigateThroughGoToRelyingOnAutoSessionSwitch()
     {
         // Navigate to "Products" page in scope of the first session.
